6d0293eea36aed682cd1a1db2763aa55ef5cfb32
Vikashkumar81021 <vikas@sestinfotech.com>
2026-08-21 10:06:42 +0530

    add factory desgin pattern

---
+++ Design Pattern/factory design pattern/simple-factory-design-pattern.js
@@ -0,0 +1,39 @@
+class Burger {
+  prepare() {}
+}
+
+class BasicBurge extends Burger {
+  prepare() {
+    console.log("Basic Burger is prepare");
+  }
+}
+class StandardBurger extends Burger {
+  prepare() {
+    console.log("Standard Burger is prepare");
+  }
+}
+
+class PremireBurger extends Burger {
+  prepare() {
+    console.log("Premimure Burger is prepare");
+  }
+}
+
+class BurgerFactory {
+  create(type) {
+    if (type === "basic") {
+      return new BasicBurge();
+    } else if (type === "standrd") {
+      return new StandardBurger();
+    } else if (type === "premiure") {
+      return new PremireBurger();
+    } else {
+      throw new Error("Invalid Type");
+    }
+  }
+}
+const type = "basic";
+const burgerFactory = new BurgerFactory();
+
+const burger = burgerFactory.create(type);
+burger.prepare();
